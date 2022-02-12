c1c380e9e64017a456da145f0944b94099287a63
Jared Tjahjadi <jared.tjahjadi@stonybrook.edu>
2022-02-12 14:08:10 -0500

    Calendar grid + colors

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,11 +1,57 @@
-import './styles.css';
+import './Styles.css';
 
+var entry = false;
 function Calendar() {
+  const ROW_LENGTH = 31
+  const COL_LENGTH = 12
+
+  const grid = [];
+  for (let row = 0; row < ROW_LENGTH; row++) {
+    const currRow = [];
+    for (let col = 0; col < COL_LENGTH; col++) {
+      currRow.push(new Square(row, col));
+    }
+    grid.push(currRow);
+  }
+
   return (
-    <div className="App">
-      Pixel Mood
+    <div className="Calendar">
+       <div className="grid">
+          {grid.map((row) => {
+            return (
+              <div key={row.row}>
+                {row.map((square) => {
+                  return (
+                    <div key = {square.col} className = "square" onClick= {Entry}></div>
+                );}
+                )}
+              </div>
+            )})}
+        </div>
     </div>
   );
+}
+
+function Entry() {
+  entry = true
+  return (
+    <div className="App">
+      whee
+      How are you feeling today?
+      <br />
+      <br />
+      <textarea />
+    </div>
+  );
+}
+
+class Square
+{
+  constructor (row, col)
+  {
+    this.row = row
+    this.col = col
+  }
 }
 
 export default Calendar;
