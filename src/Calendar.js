90eab67d9c92c4ccaca6b43ee63bbcc590db4791
amelia rae orodio <ameliarae.orodio@stonybrook.edu>
2022-02-12 21:35:57 -0500

    figuring out the onclick issue

--- src/Calendar.js
+++ src/Calendar.js
@@ -48,52 +48,52 @@ function Calendar() {
         How are you feeling today?
         <br />
         <ul className="feelings">
-          <li id="happy" onClick={() => setEntry(entry - 1)}>
+          <li id="happy">
             <img alt='Happy' src={'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/grinning-face_1f600.png'} />
             <br />
             Happy
           </li>
-          <li id="calm" onClick={() => setEntry(entry - 1)}>
+          <li id="calm">
             <img alt='Calm' src={'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/relieved-face_1f60c.png'} />
             <br />
             Calm
           </li>
-          <li id="angry" onClick={() => setEntry(entry - 1)}>
+          <li id="angry">
             <img alt='Angry' src={'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/pouting-face_1f621.png'} />
             <br />
             Angry
           </li>
-          <li id="surprised" onClick={() => setEntry(entry - 1)}>
+          <li id="surprised">
             <img alt='' src={'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/fearful-face_1f628.png'} />
             <br />
             Surprised
           </li>
-          <li id="sadness" onClick={() => setEntry(entry - 1)}>
+          <li id="sadness">
             <img alt='' src={'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/pensive-face_1f614.png'} />
             <br />
             Sadness
           </li>
-          <li id="disgust" onClick={() => setEntry(entry - 1)}>
+          <li id="disgust">
             <img alt='' src={'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/nauseated-face_1f922.png'} />
             <br />
             Disgust
           </li>
-          <li id="fear" onClick={() => setEntry(entry - 1)}>
+          <li id="fear">
             <img alt='' src={'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/fearful-face_1f628.png'} />
             <br />
             Fear
           </li>
-          <li id="sleepy" onClick={() => setEntry(entry - 1)}>
+          <li id="sleepy">
             <img alt='' src={'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/sleepy-face_1f62a.png'} />
             <br />
             Sleepy
           </li>
-          <li id="excited" onClick={() => setEntry(entry - 1)}>
+          <li id="excited">
             <img alt='' src={'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/star-struck_1f929.png'} />
             <br />
             Excited
           </li>
-          <li id="drained" onClick={() => setEntry(entry - 1)}>
+          <li id="drained">
             <img alt='' src={'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/woozy-face_1f974.png'} />
             <br />
             Drained
